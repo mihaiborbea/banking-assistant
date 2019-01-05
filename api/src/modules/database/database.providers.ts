acbb265af642accaa17318fc8d2899da561b40e1
Mihai Borbea <borbeamihai@gmail.com>
2019-01-05 22:24:57 +0200

    changed DB logs to nest logger

--- api/src/modules/database/database.providers.ts
+++ api/src/modules/database/database.providers.ts
@@ -1,22 +1,24 @@
 
 import * as mongoose from 'mongoose';
+import { Logger } from '@nestjs/common'
 
 import Config, { DBConfig } from 'src/config/config';
 
 const DBConfig: DBConfig = Config.db;
 const DBCredentials: string = DBConfig.password ? `${DBConfig.username}:${DBConfig.password}@` : '';
 const DBConnectionURI: string = `mongodb://${DBCredentials}${DBConfig.host}:${DBConfig.port}/${DBConfig.database}`
+const logger = new Logger('DatabaseModule');
 
 export const DatabaseProviders = [
   {
     provide: 'DBConnectionToken',
     useFactory: async (): Promise<typeof mongoose> => {
       try {
-        console.log(`Connecting to MongoDB on ${DBConfig.host}:${DBConfig.port}...`)
+        logger.log(`Connecting to MongoDB...`)
         await mongoose.connect(DBConnectionURI, { useNewUrlParser: true })
-        console.log(`Connected to MongoDB!`);
+        logger.log(`Connected to MongoDB!`);
       } catch (error) {
-        console.log(`Failed to connect to MongoDb.\nError:\n ${error}`);
+        logger.error(`Failed to connect to MongoDb.\nError:\n ${error}`);
       }
     }
   },
