f94f458d7375fba1b36045b739faad32338845b5
Mihai Borbea <mihai.borbea@anais.digital>
2019-01-21 01:41:18 +0200

    updated nav bar

--- client/src/modules/fuse-config/index.ts
+++ client/src/modules/fuse-config/index.ts
@@ -1,38 +1,30 @@
 import { FuseConfig } from '@fuse/types';
 
-/**
- * Default Fuse Configuration
- *
- * You can edit these options to change the default options. All these options also can be changed per component
- * basis. See `app/main/pages/authentication/login/login.component.ts` constructor method to learn more
- * about changing these options per component basis.
- */
-
 export const fuseConfig: FuseConfig = {
-    layout: {
-        style: 'horizontal-layout-1',
-        width: 'fullwidth',
-        navbar: {
-            background: 'mat-fuse-dark-700-bg',
-            folded: false,
-            hidden: false,
-            position: 'top',
-            variant: 'vertical-style-1'
-        },
-        toolbar: {
-            background: 'mat-white-500-bg',
-            hidden: false,
-            position: 'above'
-        },
-        footer: {
-            background: 'mat-fuse-dark-900-bg',
-            hidden: true,
-            position: 'below-fixed'
-        },
-        sidepanel: {
-            hidden: true,
-            position: 'right'
-        }
+  layout: {
+    style: 'horizontal-layout-1',
+    width: 'fullwidth',
+    navbar: {
+      background: 'mat-fuse-dark-700-bg',
+      folded: false,
+      hidden: false,
+      position: 'top',
+      variant: 'vertical-style-1'
     },
-    customScrollbars: true
+    toolbar: {
+      background: 'mat-white-500-bg',
+      hidden: false,
+      position: 'above'
+    },
+    footer: {
+      background: 'mat-fuse-dark-900-bg',
+      hidden: true,
+      position: 'below-fixed'
+    },
+    sidepanel: {
+      hidden: true,
+      position: 'right'
+    }
+  },
+  customScrollbars: true
 };
