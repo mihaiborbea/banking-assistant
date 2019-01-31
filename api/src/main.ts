0a2da3fa2da07acfe0429f70b3f962d2ea40fac7
Mihai Borbea <mihai.borbea@anais.digital>
2019-02-01 01:15:17 +0200

    moved public

--- api/src/main.ts
+++ api/src/main.ts
@@ -9,10 +9,9 @@ import { AppModule } from './modules/app/app.module';
 // tslint:disable-next-line:typedef
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // TODO: enable only on dev
   app.use(cors());
-  app.useStaticAssets(join(__dirname, 'public'));
-  app.setBaseViewsDir(join(__dirname, 'public', 'client'));
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'public', 'client'));
   app.setViewEngine('html');
   await app.listen(Config.port);
 }
