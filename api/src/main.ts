55328f0cdbcfdafd228788b87947a3baf5eadffa
Mihai Borbea <mihai.borbea@anais.digital>
2019-02-01 01:37:18 +0200

    add view

--- api/src/main.ts
+++ api/src/main.ts
@@ -9,6 +9,7 @@ import { AppModule } from './modules/app/app.module';
 // tslint:disable-next-line:typedef
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // TODO: enable only on dev
   app.use(cors());
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'public', 'client'));
--- api/src/modules/app/app.controller.ts
+++ api/src/modules/app/app.controller.ts
@@ -6,7 +6,8 @@ import * as path from 'path';
 export class AppController {
   @Get()
   public root(@Res() response: Response): void {
+    console.log(path.resolve(__dirname, '..', '..', '..', 'dist', 'public', 'client', 'index.html'));
     // the homepage will load our index.html which contains angular logic
-    response.sendFile(path.resolve('../../../public/client/index.html'));
+    response.sendFile(path.resolve(__dirname, '..', '..', '..', 'dist', 'public', 'client', 'index.html'));
   }
 }
